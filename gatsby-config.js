e31b0738d13afe0ada241a3dee79e96bef8d60f9
Muhammad Rizky Yusfian Yusuf <rizky.yusfian16@gmail.com>
2023-08-19 23:31:08 +0900

    set up chakra ui

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,18 @@ module.exports = {
   siteMetadata: {
     title: `rizkyyusfian.com`,
     siteUrl: `https://www.yourdomain.tld`,
+    description: `Personal website`
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: '@chakra-ui/gatsby-plugin',
+      options: {
+        /**
+         * @property {boolean} [resetCSS=true]
+         * if false, this plugin will not use `<CSSReset />
+         */
+        resetCSS: true,
+      },
+    },
+  ],
 }
