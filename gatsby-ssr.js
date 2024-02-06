7facb323addde16c4160c2e7b0ec8210a79ad122
Muhammad Rizky Yusfian Yusuf <rizky.yusfian16@gmail.com>
2024-02-07 00:29:24 +0700

    reset repo

--- gatsby-ssr.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
-import theme from "./src/styles/theme";
-import "./src/styles/global.css";
-
-export function wrapRootElement({ element }) {
-  return <ChakraProvider theme={theme}>{element}</ChakraProvider>;
-}
--- src/@chakra-ui/gatsby-plugin/theme.js
+++
@@ -1,9 +0,0 @@
-// src/@chakra-ui/gatsby-plugin/theme.js
-import { extendTheme } from '@chakra-ui/react'
-const theme = {
-  colors: {
-    primary: 'rebeccapurple',
-  },
-}
-
-export default extendTheme(theme)
