58aff7184cad26fa6d144334594031ce0156b4fc
Muhammad Rizky Yusfian Yusuf <rizky.yusfian16@gmail.com>
2024-02-08 00:00:55 +0700

    install chakra-ui

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,25 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `rizkyyusfian.com`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `rizkyyusfian - Personal Blog`,
+    siteUrl: `https://www.rizkyyusfian.com`,
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: '@chakra-ui/gatsby-plugin',
+      options: {
+        /**
+         * @property {boolean} [resetCSS=true]
+         * if false, this plugin will not use `<CSSReset />
+         */
+        resetCSS: true,
+        /**
+         * @property {number} [portalZIndex=undefined]
+         * The z-index to apply to all portal nodes. This is useful
+         * if your app uses a lot z-index to position elements.
+         */
+        portalZIndex: undefined,
+      },
+    }
+  ],
 }
